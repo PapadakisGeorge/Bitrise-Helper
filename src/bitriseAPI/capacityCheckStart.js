5920d368353bf80d8dd3242957976f7d6372911c
George Papadakis <george.papadakis@camelotls.com>
2022-01-14 16:24:30 +0200

    Android build will trigger ios build, so ww had to make the check reflect that. (#5)

--- src/bitriseAPI/capacityCheckStart.js
+++ src/bitriseAPI/capacityCheckStart.js
@@ -10,7 +10,7 @@ const _ = require('lodash');
 const {forEach} = require('p-iteration');
 const {getBuildAverageRunTime, approximateFinish} = require('../utils/runTime');
 const {getData} = require('./dataFetcher');
-const { WORKFLOWS, YES_NO_OPTIONS, YES_OPTIONS, NO_OPTIONS } = require('../model/model');
+const { WORKFLOWS, YES_NO_OPTIONS, YES_OPTIONS, NO_OPTIONS, RESERVED_SESSIONS } = require('../model/model');
 const {triggerStart} = require('./triggerStart');
 
 const capacityCheckStart = async () => {
@@ -28,19 +28,7 @@ const capacityCheckStart = async () => {
     }
     // Start the actual work.
     let workflowData = {}
-
-    const reservedSessions = {
-        Workflow_Android: 15,
-        Workflow_IOS: 20,
-        Workflow_Android_Edge: 10,
-        Workflow_IOS_Edge: 30,
-        Execute_Android_Tests: 15,
-        Execute_Android_Edge_Tests: 10,
-        Execute_IOS_Tests: 20,
-        Execute_IOS_Edge_Suite_1_Tests: 15,
-        Execute_IOS_Edge_Suite_2_Tests: 15,
-    }
-
+    
     const BITRISE_BUILDS_URL = 'https://api.bitrise.io/v0.1/apps/af50b4926a122ad0/builds';
     const SESSIONS_LIMIT = '70';
 
@@ -48,7 +36,7 @@ const capacityCheckStart = async () => {
         const approximateTriggerTime = await getBuildAverageRunTime(workflow);
         workflowData[workflow] = {
             approximateTriggerTime: approximateTriggerTime,
-            reservedSessions: reservedSessions[workflow]
+            RESERVED_SESSIONS: RESERVED_SESSIONS[workflow]
         };
     }
 //Get builds that are currently running, in how much time will the build end, and return the number of sessions will use when my builds will start.
@@ -57,7 +45,7 @@ const capacityCheckStart = async () => {
 
     const buildsRunningRawData = await getData(BITRISE_BUILDS_URL, [['status', 0]]);
     const buildsRunning = JSON.parse(buildsRunningRawData.body).data;
-    const filteredBuilds = buildsRunning.filter((build) => Object.keys(reservedSessions).includes(build.triggered_workflow));
+    const filteredBuilds = buildsRunning.filter((build) => Object.keys(RESERVED_SESSIONS).includes(build.triggered_workflow));
 
     await forEach(filteredBuilds, async (build) => await getTimeAndReservedSessions(build.triggered_workflow));
 
@@ -65,7 +53,7 @@ const capacityCheckStart = async () => {
     let buildsThatAffect = {};
     const saveBuildData = (build, approximateEndTime) => {
         buildsThatAffect[`${build.build_number}`] = {
-            sessions: workflowData[build.triggered_workflow].reservedSessions,
+            sessions: workflowData[build.triggered_workflow].RESERVED_SESSIONS,
             buildName: build.triggered_workflow,
             finishTime: approximateEndTime,
         };
@@ -90,11 +78,11 @@ const capacityCheckStart = async () => {
         const buildName = build.triggered_workflow;
         const approximateEndTime = await approximateFinish(build.triggered_at, buildName);
         if (buildName.includes('Execute') && Number(approximateEndTime) > Number(workflowData[WORKFLOW].approximateTriggerTime)) {
-            occupiedSessions += workflowData[buildName].reservedSessions;
+            occupiedSessions += workflowData[buildName].RESERVED_SESSIONS;
             saveBuildData(build, approximateEndTime);
         } else if (buildName.includes('Workflow')) {
             if (!checkIfSkipTestsWasUsed(build)) {
-                occupiedSessions += Number(workflowData[buildName].reservedSessions);
+                occupiedSessions += Number(workflowData[buildName].RESERVED_SESSIONS);
                 saveBuildData(build, approximateEndTime);
             }
         }
@@ -106,10 +94,11 @@ const capacityCheckStart = async () => {
         console.log(consoleCyan, `${buildsThatAffect[buildNumber].buildName} with build number ${buildNumber} ${reserveTense} ${buildsThatAffect[buildNumber].sessions} sessions and will end ${finishTimeText}`);
     });
 
-    if (occupiedSessions + Number(workflowData[WORKFLOW].reservedSessions) > SESSIONS_LIMIT) {
-        console.log(consoleRed, `Workflow ${WORKFLOW} should not be triggered at the moment :( Estimated sessions (with your build) ${occupiedSessions + Number(workflowData[WORKFLOW].reservedSessions)}`);
+    const sessionsYouWillNeed = WORKFLOW === 'Workflow_Android' ? Number(workflowData['Workflow_Android'].RESERVED_SESSIONS) + Number(workflowData['Workflow_IOS'].RESERVED_SESSIONS) : Number(workflowData[WORKFLOW].RESERVED_SESSIONS);
+    if (occupiedSessions + sessionsYouWillNeed > SESSIONS_LIMIT) {
+        console.log(consoleRed, `Workflow ${WORKFLOW} will need ${sessionsYouWillNeed}, so it should not be triggered at the moment :( Estimated sessions (with your build) ${occupiedSessions + Number(workflowData[WORKFLOW].RESERVED_SESSIONS)}`);
     } else {
-        console.log(consoleGreen, `${WORKFLOW} can be triggered!!!`);
+        console.log(consoleGreen, `${WORKFLOW} will need ${sessionsYouWillNeed}, so it can be triggered!!!`);
         let shouldTriggerBuild;
         while (!shouldTriggerBuild) {
             shouldTriggerBuild = readline.question(`Would you like to trigger the ${WORKFLOW}? (y/n):\n`, {
--- src/model/model.js
+++ src/model/model.js
@@ -9,9 +9,22 @@ const YES_OPTIONS = ['yes','Yes','YES','Y','y'];
 const NO_OPTIONS = ['no','No','NO','N','n'];
 const YES_NO_OPTIONS = [...YES_OPTIONS,...NO_OPTIONS];
 
+const RESERVED_SESSIONS = {
+    Workflow_Android: 15,
+    Workflow_IOS: 20,
+    Workflow_Android_Edge: 10,
+    Workflow_IOS_Edge: 30,
+    Execute_Android_Tests: 15,
+    Execute_Android_Edge_Tests: 10,
+    Execute_IOS_Tests: 20,
+    Execute_IOS_Edge_Suite_1_Tests: 15,
+    Execute_IOS_Edge_Suite_2_Tests: 15,
+}
+
 module.exports= {
     WORKFLOWS,
     YES_OPTIONS,
     NO_OPTIONS,
-    YES_NO_OPTIONS
+    YES_NO_OPTIONS,
+    RESERVED_SESSIONS
 }
