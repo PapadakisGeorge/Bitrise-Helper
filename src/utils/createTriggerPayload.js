e7753b6583d7f37724c22c1de6f9b317fb5f7f10
PapadakisGeorge <george.papadakis@camelotls.com>
2022-01-19 13:11:36 +0200

    Removed import no longer needed

--- src/utils/createTriggerPayload.js
+++ src/utils/createTriggerPayload.js
@@ -1,5 +1,4 @@
 const {validateInputs} = require('./validateInputs');
-const {CONSOLE_RED} = require('../model/model');
 
 /**
  *
@@ -9,26 +8,21 @@ const {CONSOLE_RED} = require('../model/model');
  * @returns {{build_params: {base_repository_url: string, workflow_id: string, environments, branch: string}, hook_info: {type: string}}|*} Returns a valid payload.
  */
 const createTriggerPayload = (branch, workflow, envVariables) => {
-    try {
-        validateInputs([
-            {value: branch, desiredType: 'string'},
-            {value: workflow, desiredType: 'string'},
-            {value: envVariables, desiredType: 'array'}
-        ]);
-        return {
-            "hook_info": {
-                "type": "bitrise"
-            },
-            "build_params": {
-                "branch": `${branch}`,
-                "workflow_id": `${workflow}`,
-                "base_repository_url": "https://github.com/camelotls/ie-native-app",
-                "environments": envVariables
-            }
+    validateInputs([
+        {value: branch, desiredType: 'string'},
+        {value: workflow, desiredType: 'string'},
+        {value: envVariables, desiredType: 'array'}
+    ]);
+    return {
+        "hook_info": {
+            "type": "bitrise"
+        },
+        "build_params": {
+            "branch": `${branch}`,
+            "workflow_id": `${workflow}`,
+            "base_repository_url": "https://github.com/camelotls/ie-native-app",
+            "environments": envVariables
         }
-    } catch (error) {
-        console.log(CONSOLE_RED, 'Error while creating the payload');
-        return error;
     }
 }
 
