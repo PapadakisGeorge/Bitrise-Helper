c0f2648a351adfb186047f42a026f547b6f25c1e
PapadakisGeorge <george.papadakis@camelotls.com>
2022-01-24 10:34:45 +0200

    Added option to run a workflow using a specific test suite

--- src/bitriseAPI/triggerStart.js
+++ src/bitriseAPI/triggerStart.js
@@ -1,78 +1,120 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 const {
-    consoleBlue,
-    consoleRed, consoleGreen,
-} = require('../utils/consoleColors');
-const {WORKFLOWS, YES_NO_OPTIONS, YES_OPTIONS, NO_OPTIONS} = require('../model/model');
+  consoleBlue,
+  consoleRed,
+  consoleGreen,
+} = require("../utils/consoleColors");
+const {
+  WORKFLOWS,
+  YES_NO_OPTIONS,
+  YES_OPTIONS,
+  NO_OPTIONS,
+  TEST_SUITES,
+} = require("../model/model");
 
-const {triggerBuild} = require("./triggerBuild");
-const {watcherStart} = require("./watcherStart");
-const {createPayload} = require("../utils/createPayload");
+const { triggerBuild } = require("./triggerBuild");
+const { watcherStart } = require("./watcherStart");
+const { createPayload } = require("../utils/createPayload");
 
-const triggerStart = async (initialWorkflow = '') => {
-    let BRANCH;
-    let WORKFLOW = initialWorkflow;
-    let SKIP_TESTS;
-    const availableWorkflowsMatrix = Object.values(WORKFLOWS);
-    while (!BRANCH) {
-        BRANCH = readline.question(`Enter the branch name you want to trigger:\n`,);
-        if (!BRANCH) {
-            console.log(consoleRed, 'You need to specify a branch!')
+const triggerStart = async (initialWorkflow = "") => {
+  let BRANCH;
+  let WORKFLOW = initialWorkflow;
+  let SKIP_TESTS;
+  let TEST_SUITE_TAG = "";
+
+  const availableWorkflowsMatrix = Object.values(WORKFLOWS);
+  while (!BRANCH) {
+    BRANCH = readline.question(`Enter the branch name you want to trigger:\n`);
+    if (!BRANCH) {
+      console.log(consoleRed, "You need to specify a branch!");
+    }
+    if (!initialWorkflow) {
+      while (!WORKFLOW) {
+        let WORKFLOW_INPUT = readline.keyInSelect(
+          availableWorkflowsMatrix,
+          `Which workflow do you want to trigger?`
+        );
+        if (WORKFLOW_INPUT === -1) {
+          console.log(consoleBlue, "Aborting trigger...");
+          process.exit(0);
         }
-        if (!initialWorkflow) {
-            while (!WORKFLOW) {
-                let WORKFLOW_INPUT = readline.keyInSelect(availableWorkflowsMatrix, `Which workflow do you want to trigger?`);
-                if (WORKFLOW_INPUT === -1) {
-                    console.log(consoleBlue, 'Aborting trigger...');
-                    process.exit(0)
-                }
-                WORKFLOW = availableWorkflowsMatrix[WORKFLOW_INPUT];
-            }
-        }
-        while (!SKIP_TESTS) {
-            SKIP_TESTS = readline.question(`Skip tests? (y/n):\n`, {
-                limit: YES_NO_OPTIONS,
-                limitMessage: `Type y or n!`
-            });
-        }
-
-
-        const skippingTestsText = YES_OPTIONS.includes(SKIP_TESTS) ? 'Tests will be skipped' : 'Tests will run'
-        console.log(consoleBlue, `Triggering ${WORKFLOW} for branch ${BRANCH}. ${skippingTestsText}`);
+        WORKFLOW = availableWorkflowsMatrix[WORKFLOW_INPUT];
+      }
+    }
+    while (!SKIP_TESTS) {
+      SKIP_TESTS = readline.question(`Skip tests? (y/n):\n`, {
+        limit: YES_NO_OPTIONS,
+        limitMessage: `Type y or n!`,
+      });
     }
 
-    const envVariables = [
+    const skippingTestsText = YES_OPTIONS.includes(SKIP_TESTS)
+      ? "Tests will be skipped"
+      : "Tests will run";
+    if (NO_OPTIONS.includes(SKIP_TESTS)) {
+      let TEST_SUITE;
+      while (!TEST_SUITE) {
+        TEST_SUITE = readline.keyInSelect(
+          TEST_SUITES,
+          `Do you want to run a specific test suite?\n`
+        );
+      }
+      if (TEST_SUITE === -1) {
+        console.log(consoleBlue, "Will not run a specific test suite.");
+      } else {
+        TEST_SUITE_TAG = `@${TEST_SUITES[TEST_SUITE]}`;
+      }
+    }
+    console.log(
+      consoleBlue,
+      `Triggering ${WORKFLOW} for branch ${BRANCH}. ${skippingTestsText}.${
+        TEST_SUITE_TAG ? `Running tests with tag ${TEST_SUITE_TAG}` : ""
+      }`
+    );
+  }
+
+  const envVariables = [
+    {
+      is_expand: true,
+      mapped_to: "SKIP_TESTS",
+      value: YES_OPTIONS.includes(SKIP_TESTS) ? "true" : "false",
+    },
+    {
+      mapped_to: "TEST_SUITE_NAME",
+      value: TEST_SUITE_TAG,
+    },
+  ];
+  const payload = createPayload(BRANCH, WORKFLOW, envVariables);
+
+  let response = await triggerBuild(payload);
+  if (response.statusCode > 201) {
+    console.log(consoleRed, "Something went wrong, try again :(");
+  } else {
+    console.log(
+      consoleGreen,
+      `Build triggered successfully, more info: ${
+        JSON.parse(response.body).build_url
+      }`
+    );
+    let shouldWatchBuilds;
+    while (!shouldWatchBuilds) {
+      shouldWatchBuilds = readline.question(
+        `Would you like to watch the build? (y/n):\n`,
         {
-            "is_expand": true,
-            "mapped_to": "SKIP_TESTS",
-            "value": YES_OPTIONS.includes(SKIP_TESTS) ? "true" : "false"
-        },
-    ]
-    const payload = createPayload(BRANCH, WORKFLOW, envVariables);
-
-    let response = await triggerBuild(payload);
-    if
-    (response.statusCode > 201
-    ) {
-        console.log(consoleRed, 'Something went wrong, try again :(')
-    } else {
-        console.log(consoleGreen, `Build triggered successfully, more info: ${JSON.parse(response.body).build_url}`);
-        let shouldWatchBuilds;
-        while (!shouldWatchBuilds) {
-            shouldWatchBuilds = readline.question(`Would you like to watch the build? (y/n):\n`, {
-                limit: YES_NO_OPTIONS,
-                limitMessage: `Type y or n!`
-            });
+          limit: YES_NO_OPTIONS,
+          limitMessage: `Type y or n!`,
         }
-
-        if (NO_OPTIONS.includes(shouldWatchBuilds)) {
-            process.exit(0);
-        } else if (YES_OPTIONS.includes(shouldWatchBuilds)) {
-            await watcherStart(BRANCH)
-        }
+      );
     }
-}
+
+    if (NO_OPTIONS.includes(shouldWatchBuilds)) {
+      process.exit(0);
+    } else if (YES_OPTIONS.includes(shouldWatchBuilds)) {
+      await watcherStart(BRANCH);
+    }
+  }
+};
 
 module.exports = {
-    triggerStart
-}
+  triggerStart,
+};
