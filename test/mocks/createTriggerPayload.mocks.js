fc89d77f72515e4da4dc7fb2e8dae670aed8e0ce
PapadakisGeorge <george.papadakis@camelotls.com>
2022-01-31 17:24:44 +0200

    Merge changes

--- test/mocks/createTriggerPayload.mocks.js
+++ test/mocks/createTriggerPayload.mocks.js
@@ -1,77 +1,61 @@
-const {ValidationError} = require('../../src/utils/validateInputs');
+const { ValidationError } = require("../../src/utils/validateInputs");
 
 const VALID_INPUTS = {
-    branch: 'Valid branch input',
-    workflow: 'Valid workflow input',
-    envVariables: ['Valid workflow env variable 1', 'Valid workflow env variable 2'],
-    expectedResult: {
-        "hook_info": {
-            "type": "bitrise"
-        },
-        "build_params": {
-            "branch": "Valid branch input",
-            "workflow_id": "Valid workflow input",
-            "base_repository_url": "https://github.com/camelotls/ie-native-app",
-            "environments": ['Valid workflow env variable 1', 'Valid workflow env variable 2']
-        }
+  branch: "Valid branch input",
+  workflow: "Valid workflow input",
+  envVariables: [
+    "Valid workflow env variable 1",
+    "Valid workflow env variable 2",
+  ],
+  expectedResult: {
+    hook_info: {
+      type: "bitrise",
     },
-    schema: {
-        type: 'object',
+    build_params: {
+      branch: "Valid branch input",
+      workflow_id: "Valid workflow input",
+      base_repository_url: "https://github.com/camelotls/ie-native-app",
+      environments: [
+        "Valid workflow env variable 1",
+        "Valid workflow env variable 2",
+      ],
+    },
+  },
+  schema: {
+    type: "object",
+    properties: {
+      hook_info: {
+        type: "object",
         properties: {
-            hook_info: {
-                type: 'object',
-                properties: {
-                    type: {
-                        type: 'string',
-
-                    }
-                }
+          type: {
+            type: "string",
+          },
+        },
+      },
+      build_params: {
+        type: "object",
+        properties: {
+          branch: {
+            type: "string",
+          },
+          workflow_id: {
+            type: "string",
+          },
+          base_repository_url: {
+            type: "string",
+          },
+          environments: {
+            type: "array",
+            items: {
+              type: "string",
             },
-            build_params: {
-                type: 'object',
-                properties: {
-                    branch: {
-                        type: 'string'
-                    },
-                    workflow_id: {
-                        type: 'string'
-                    },
-                    base_repository_url: {
-                        type: 'string'
-                    },
-                    environments: {
-                        type: 'array',
-                        items: {
-                            type: 'string'
-                        }
-                    },
-                }
-            }
-        }
-    }
-}
-
-
-const INVALID_INPUTS = [{
-    branch: 'Valid branch input',
-    workflow: 'Valid workflow input',
-    envVariables: 'String is an invalid input',
-    expectedResult: new ValidationError(`\nInput: String is an invalid input\nType: not array`)
-}, {
-    branch: ['Array is an invalid input'],
-    workflow: 'Valid workflow input',
-    envVariables: ['Array is valid'],
-    expectedResult: new ValidationError(`\nInput: Array is an invalid input\nType: not string`)
-}, {
-
-    branch: 'Valid branch input',
-    workflow: 1,
-    envVariables: ['Array is valid'],
-    expectedResult: new ValidationError(`\nInput: 1\nType: not string`)
-}
-]
+          },
+        },
+      },
+    },
+  },
+};
 
 module.exports = {
-    VALID_INPUTS,
-    INVALID_INPUTS
-}
+  VALID_INPUTS,
+};
--- test/mocks/spinners.mocks.js
+++ test/mocks/spinners.mocks.js
@@ -1,178 +1,186 @@
-const {ValidationError} = require('../../src/utils/validateInputs');
-
 const SPINNER_TEXT_HAPPY_PATH_INPUTS = [
-    {
-        build: {
-            triggered_workflow: 'A valid workflow',
-            slug: 'aValidSlug',
-            build_number: '6666'
-        },
-        finishTime: '2',
-        expectedResult: 'A valid workflow workflow (build number 6666) in progress, ETC 2 minutes. More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
+  {
+    build: {
+      triggered_workflow: "A valid workflow",
+      slug: "aValidSlug",
+      build_number: "6666",
+      branch: "valid branch",
     },
-    {
-        build: {
-            triggered_workflow: 'A valid workflow',
-            slug: 'aValidSlug',
-            build_number: '6666'
-        },
-        finishTime: '1',
-        expectedResult: 'A valid workflow workflow (build number 6666) in progress, will finish soon. More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
+    finishTime: "2",
+    expectedResult:
+      "A valid workflow workflow for valid branch (build number 6666) in progress, ETC 2 minutes.\nMore info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
+  },
+  {
+    build: {
+      triggered_workflow: "A valid workflow",
+      slug: "aValidSlug",
+      build_number: "6666",
+      branch: "valid branch",
     },
-    {
-        build: {
-            triggered_workflow: 'A valid workflow',
-            slug: 'aValidSlug',
-            build_number: '6666'
-        },
-        finishTime: '-15',
-        expectedResult: 'A valid workflow workflow (build number 6666) in progress, but is taking to long. Check if everything is alright here: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
+    finishTime: "1",
+    expectedResult:
+      "A valid workflow workflow for valid branch (build number 6666) in progress, will finish soon.\nMore info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
+  },
+  {
+    build: {
+      triggered_workflow: "A valid workflow",
+      slug: "aValidSlug",
+      build_number: "6666",
+      branch: "valid branch",
     },
+    finishTime: "-15",
+    expectedResult:
+      "A valid workflow workflow for valid branch (build number 6666) in progress, but is taking to long.\nCheck if everything is alright here: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
+  },
 ];
 
 const START_SPINNER_HAPPY_PATH_INPUTS = {
-    build: {
-        triggered_workflow: 'A valid workflow',
-        slug: 'aValidSlug',
-        build_number: '6666'
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "6666",
+    branch: "valid branch",
+  },
+  finishTime: "1",
+  expectedResult: {
+    "spinner-6666": {
+      color: "cyan",
+      succeedColor: "green",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      status: "spinning",
+      text: "A valid workflow workflow for valid branch (build number 6666) in progress, will finish soon.\nMore info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
     },
-    finishTime: '1',
-    expectedResult: {
-        'spinner-6666': {
-            color: 'cyan',
-            succeedColor: 'green',
-            failColor: 'red',
-            spinnerColor: 'greenBright',
-            succeedPrefix: '✓',
-            failPrefix: '✖',
-            status: 'spinning',
-            text: 'A valid workflow workflow (build number 6666) in progress, will finish soon. More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-        }
-    }
-}
+  },
+};
 
 const UPDATE_SPINNER_HAPPY_PATH_INPUTS = {
-    build: {
-        triggered_workflow: 'A valid workflow',
-        slug: 'aValidSlug',
-        build_number: '6666'
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "6666",
+    branch: "valid branch",
+  },
+  finishTime: "3",
+  expectedResult: {
+    "spinner-6666": {
+      color: "cyan",
+      succeedColor: "green",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      status: "spinning",
+      text: "A valid workflow workflow for valid branch (build number 6666) in progress, ETC 3 minutes.\nMore info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
     },
-    finishTime: '3',
-    expectedResult: {
-        'spinner-6666': {
-            color: 'cyan',
-            succeedColor: 'green',
-            failColor: 'red',
-            spinnerColor: 'greenBright',
-            succeedPrefix: '✓',
-            failPrefix: '✖',
-            status: 'spinning',
-            text: 'A valid workflow workflow (build number 6666) in progress, ETC 3 minutes. More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-        }
-    }
-}
+  },
+};
 
-const STOP_SPINNER_SUCCESS =
-    {
-        build: {
-            triggered_workflow: 'A valid workflow',
-            slug: 'aValidSlug',
-            build_number: '6668'
-        },
-        buildNumber: '6668',
-        buildType: 'A valid workflow',
-        buildStatus: '1',
-        buildURL: 'aValidSlug',
-        expectedResult: {
-            'spinner-6668': {
-                color: 'cyan',
-                succeedColor: 'green',
-                failColor: 'red',
-                spinnerColor: 'greenBright',
-                succeedPrefix: '✓',
-                failPrefix: '✖',
-                status: 'succeed',
-                text: 'A valid workflow 6668 succeeded! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-            }
-        }
-    }
+const STOP_SPINNER_SUCCESS = {
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "6668",
+    branch: "valid branch",
+  },
+  buildNumber: "6668",
+  buildType: "A valid workflow",
+  buildStatus: "1",
+  buildURL: "aValidSlug",
+  expectedResult: {
+    "spinner-6668": {
+      color: "cyan",
+      succeedColor: "green",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      status: "succeed",
+      text: "A valid workflow 6668 succeeded! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
+    },
+  },
+};
 const STOP_SPINNER_FAILURE = {
-    build: {
-        triggered_workflow: 'A valid workflow',
-        slug: 'aValidSlug',
-        build_number: '6669'
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "6669",
+    branch: "valid branch",
+  },
+  buildNumber: "6669",
+  buildType: "A valid workflow",
+  buildStatus: "2",
+  buildURL: "aValidSlug",
+  expectedResult: {
+    "spinner-6669": {
+      color: "cyan",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      status: "succeed",
+      succeedColor: "green",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      text: "A valid workflow 6669 failed! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
     },
-    buildNumber: '6669',
-    buildType: 'A valid workflow',
-    buildStatus: '2',
-    buildURL: 'aValidSlug',
-    expectedResult: {
-        'spinner-6669': {
-            color: 'cyan',
-            failColor: 'red',
-            spinnerColor: 'greenBright',
-            status: 'fail',
-            succeedColor: 'green',
-            succeedPrefix: '✓',
-            failPrefix: '✖',
-            text: 'A valid workflow 6669 failed! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-        }
-    }
-}
+  },
+};
 const STOP_SPINNER_ABORTED = {
-    build: {
-        triggered_workflow: 'A valid workflow',
-        slug: 'aValidSlug',
-        build_number: '66610'
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "66610",
+    branch: "valid branch",
+  },
+  buildNumber: "66610",
+  buildType: "A valid workflow",
+  buildStatus: "3",
+  buildURL: "aValidSlug",
+  expectedResult: {
+    "spinner-66610": {
+      color: "cyan",
+      succeedColor: "green",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      status: "succeed",
+      text: "A valid workflow 66610 was aborted! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
     },
-    buildNumber: '66610',
-    buildType: 'A valid workflow',
-    buildStatus: '3',
-    buildURL: 'aValidSlug',
-    expectedResult: {
-        'spinner-66610': {
-            color: 'cyan',
-            succeedColor: 'green',
-            failColor: 'red',
-            spinnerColor: 'greenBright',
-            succeedPrefix: '✓',
-            failPrefix: '✖',
-            status: 'succeed',
-            text: 'A valid workflow 66610 was aborted! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-        }
-    }
-}
+  },
+};
 const STOP_SPINNER_ABORTED_WITH_SUCCESS = {
-    build: {
-        triggered_workflow: 'A valid workflow',
-        slug: 'aValidSlug',
-        build_number: '66611'
+  build: {
+    triggered_workflow: "A valid workflow",
+    slug: "aValidSlug",
+    build_number: "66611",
+    branch: "valid branch",
+  },
+  buildNumber: "66611",
+  buildType: "A valid workflow",
+  buildStatus: "4",
+  buildURL: "aValidSlug",
+  expectedResult: {
+    "spinner-66611": {
+      color: "cyan",
+      succeedColor: "green",
+      failColor: "red",
+      spinnerColor: "greenBright",
+      succeedPrefix: "✓",
+      failPrefix: "✖",
+      status: "succeed",
+      text: "A valid workflow 66611 was aborted with success! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug",
     },
-    buildNumber: '66611',
-    buildType: 'A valid workflow',
-    buildStatus: '4',
-    buildURL: 'aValidSlug',
-    expectedResult: {
-        'spinner-66611': {
-            color: 'cyan',
-            succeedColor: 'green',
-            failColor: 'red',
-            spinnerColor: 'greenBright',
-            succeedPrefix: '✓',
-            failPrefix: '✖',
-            status: 'succeed',
-            text: 'A valid workflow 66611 was aborted with success! More info: https://app.bitrise.io/app/af50b4926a122ad0#/builds/aValidSlug'
-        }
-    }
-}
-
+  },
+};
 
 module.exports = {
-    SPINNER_TEXT_HAPPY_PATH_INPUTS,
-    START_SPINNER_HAPPY_PATH_INPUTS,
-    UPDATE_SPINNER_HAPPY_PATH_INPUTS,
-    STOP_SPINNER_SUCCESS,
-    STOP_SPINNER_FAILURE,
-    STOP_SPINNER_ABORTED,
-    STOP_SPINNER_ABORTED_WITH_SUCCESS
-}
+  SPINNER_TEXT_HAPPY_PATH_INPUTS,
+  START_SPINNER_HAPPY_PATH_INPUTS,
+  UPDATE_SPINNER_HAPPY_PATH_INPUTS,
+  STOP_SPINNER_SUCCESS,
+  STOP_SPINNER_FAILURE,
+  STOP_SPINNER_ABORTED,
+  STOP_SPINNER_ABORTED_WITH_SUCCESS,
+};
